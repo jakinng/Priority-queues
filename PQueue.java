b9a832430e43a0c63f02b3d788ab84caab311416
xueenwu <45607584+xueenwu@users.noreply.github.com>
2019-06-02 23:11:34 -0400

    Add files via upload

--- PQueue.java
+++ PQueue.java
@@ -1,4 +1,6 @@
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PQueue 
 {
@@ -9,32 +11,81 @@ public class PQueue
 		elements = new ArrayList<Comparable>();
 	}
 	
+	public static void main(String[] args) 
+	{
+		Integer[] ints = new Integer[] {
+				5, 12, 12, 36, 56, 40, 21, 88, 75, 75, 62, 79, 100, 92, 87
+		};
+		PQueue pq = new PQueue();
+		for (Integer i : ints)
+		{
+			pq.add(i);
+		}
+//		
+//		System.out.println(pq.remove());
+//		System.out.println(pq);
+//		System.out.println(pq.remove());
+//		System.out.println(pq);		
+//		System.out.println(pq.remove());
+//		System.out.println(pq);
+//		System.out.println(pq.remove());
+//		System.out.println(pq);
+	}
+	
 	public void add(Comparable element)
 	{
-		elements.add(null);
+		elements.add(element);
 		int index = elements.size() - 1;
 		
-		while (index > 1 && getParent(index).compareTo(element) > 0)
+		int parent = (index - 1) / 2;
+		while (index > 0 && 
+				element.compareTo(elements.get(parent)) < 0)
 		{
-			elements.set(index, getParent(index));
-			index = index / 2; //index set to the index of its parent
+			elements.set(index, elements.get(parent));
+			index = parent;
+			parent = (index - 1) / 2;
 		}
+		
+		elements.set(index, element);
+	}
+
+	public Comparable remove()
+	{
+		Comparable smallest = elements.get(0);
+		Comparable last = elements.remove(elements.size() - 1);
+		elements.set(0, last);
+		
+		int index = 0;
+		int child = smallestChild(index);
+		
+		while (child < elements.size() && 
+				last.compareTo(elements.get(child)) > 0)
+		{
+			elements.set(index, elements.get(child));
+			index = child;
+			child = smallestChild(index);
+		}
+		
+		elements.set(index, last);
+		return smallest;
 	}
 	
-	
-	
-	private Comparable getLeftChild(int index) 
+	private int smallestChild(int index)
 	{
-		return elements.get(2 * index);
+		int child = index * 2 + 1;
+		if (child + 1 < elements.size() && 
+				elements.get(child).compareTo(elements.get(child + 1)) > 0)
+			child = child + 1;
+		return child;
 	}
 	
-	private Comparable getRightChild(int index)
+	public Comparable peek()
 	{
-		return elements.get(2 * index + 1);
+		return elements.get(0);
 	}
-	
-	private Comparable getParent(int index) 
+
+	public String toString()
 	{
-		return elements.get(index / 2);
+		return elements.toString();
 	}
 }
